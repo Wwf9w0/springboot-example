fbb51e460997f4bb6b30a77612a59e5e8aee2596
emrekaradag <aliemrekaradag1@gmail.com>
2021-08-26 02:31:40 +0300

    rest service content

---
+++ spring-boot-2-rest-service-content-negotiation/src/main/java/com/frixie0/springboot/rest/example/springboot2restservicecontentnegotiation/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.frixie0.springboot.rest.example.springboot2restservicecontentnegotiation.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
